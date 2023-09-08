13861357aca754a78cd012519ca1b71592d928d5
noel1432 <85392319+noel1432@users.noreply.github.com>
2023-09-08 10:10:55 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,4 +6,13 @@ function displayProduct() {
   document.getElementById("answer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
 }
 
+function displayDiff() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum - secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
+}
+
 document.getElementById('multiplyButton').addEventListener("click", displayProduct);
+document.getElementById('subtractButton').addEventListener("click", displayDiff);
