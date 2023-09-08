747b95184c8e3254c6a223f64edd03463c2c0a38
noel1432 <85392319+noel1432@users.noreply.github.com>
2023-09-08 10:24:23 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ function displayProduct() {
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
   let total = firstNum * secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
+  document.getElementById("Productanswer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
 }
 
 function displayDiff() {
@@ -11,7 +11,7 @@ function displayDiff() {
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
   let total = firstNum - secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
+  document.getElementById("Diffanswer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
 }
 
 document.getElementById('multiplyButton').addEventListener("click", displayProduct);
